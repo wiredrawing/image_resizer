f5accf15ea6cdb325100e5204201638b73e140ef
Senbiki-Akifumi <a-senbiki@sunmark.bz>
2022-07-02 22:21:08 +0900

    画像リサイザーツール

---
+++ app_image.js
@@ -0,0 +1,28 @@
+// https://localhost/image/~のルーティング処理
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/image/index");
+const path = require("path");
+const fs = require("fs");
+// 画像リサイズツールの事前処理
+const originalFilePath = path.resolve(__dirname, "./original_path");
+// リサイズ後のディレクト
+const resizedFilePath = path.resolve(__dirname, "./resized_path");
+if (fs.existsSync(originalFilePath) !== true) {
+  fs.mkdir(originalFilePath, (error) => {
+    if (error !== null) {
+      console.log(error);
+    }
+  })
+}
+if (fs.existsSync(resizedFilePath) !== true) {
+  fs.mkdir(resizedFilePath, (error) => {
+    if (error !== null) {}
+    console.log(error);
+  })
+}
+app.use("/image", indexRouter);
+
+
+
+module.exports = app;
---
+++ routes/image/index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const router = express.Router();
+const glob = require("glob");
+const path = require("path")
+const sharp = require("sharp");
+const {check, validationResult} = require("express-validator");
+router.get("/", function(req, res, next) {
+  console.log("http://localhost:3000/image/");
+});
+/**
+ * 指定したディレクトリ内に存在する画像ファイルすべてを指定の画像サイズに
+ * リサイズして別名保存する
+ */
+router.get("/resize", [
+    check("width").isInt({min:10, max: 1000})
+  ],
+  function(req, res, next) {
+  // リサイズ後のwidth
+  let resizedWidth = req.query.width || "100";
+  console.log(resizedWidth);
+  // 元ファイルの画像パス
+  const imageFilePath = path.resolve(__dirname, "../../original_path");
+  // リサイズ後の画像パス
+  const resizedFilePath = path.resolve(__dirname, "../../resized_path/");
+
+  const readFiles = function() {
+    return new Promise(function(resolve, reject) {
+      console.log(imageFilePath);
+      let options = {
+        // スキャンする基準のディレクトリをcwdプロパティで指定
+        cwd: imageFilePath,
+        // フルパスで取得したい場合は trueに
+        absolute: false,
+      };
+      glob("*.+(jpeg|jpg|png)", options, function(error, files) {
+        if ( error !== null ) {
+          reject(error);
+          return false;
+        }
+        resolve(files);
+        return true;
+      });
+    });
+  }
+
+  const resize = async function() {
+    try {
+      let files = await readFiles();
+      let number = files.length;
+      let completedNumber = 0;
+      // 無名関数でawaitする
+      return await (() => {
+        return new Promise(function(resolve, reject) {
+          files.forEach(function(file, index) {
+            // オリジナルファイルのフルパス
+            let fullPath = path.resolve(imageFilePath, file);
+            // リサイズ後のファイル保存先
+            let resizeFullPath = path.resolve(resizedFilePath, file);
+            sharp(fullPath).resize(parseInt(resizedWidth, 10)).toFile(resizeFullPath).then(function(data) {
+
+              completedNumber++;
+              if ( completedNumber === number ) {
+                resolve(files);
+              }
+            }).catch(function(error) {
+              console.log(error);
+            })
+          })
+        })
+      })();
+    } catch (error) {
+      console.log(error)
+      return Promise.reject(error);
+    }
+  }
+  return resize().then(function(data) {
+    console.log(data);
+    res.write("completed");
+    return res.end();
+  }).catch(function(error) {
+    console.log(error);
+  })
+})
+
+module.exports = router
